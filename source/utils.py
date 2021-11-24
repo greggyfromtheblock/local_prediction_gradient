7c4bbd2874fcea490d280a1e0a5db508181cb70d
greggyfromtheblock <greg.pascalis@gmail.com>
2021-11-24 16:11:53 +0100

    Add function to create correlated variable from an existing data

--- source/utils.py
+++ source/utils.py
@@ -2,6 +2,8 @@ import os
 import torch
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import scale
+import statsmodels.api as stats
 
 def attribution2df(attributions, feature_names, loader):
     """
@@ -37,3 +39,49 @@ def log_global_importance(trainer, dataframe, name='', experiment_name='', n=0, 
         os.mkdir(f'/data/analysis/ag-reils/ag-reils-shared/cardioRS/results/interpretability/cor/{experiment_name}/global')
 
     dataframe.to_csv(f'/data/analysis/ag-reils/ag-reils-shared/cardioRS/results/interpretability/cor/{experiment_name}/global/{name}-{experiment_name}-n{n}-{seed}.csv')
+
+def sample_from_population(n=100, mu=0, sd=1, r=0):
+    """
+    :param n: number of samples
+    :param mu: scale parameter
+    :param sd: standard deviation
+    :param r: correlation coefficient
+    :return:
+    """
+    # sample r from distribution depending on n and r
+    r_sd = np.sqrt(1/n) * (1-r**2)
+    sample_r = np.random.normal(loc=r, scale=r_sd)
+    sample_r = -1 if sample_r < -1 else sample_r
+    sample_r = 1 if sample_r > 1 else sample_r
+
+    # sample mu from distribution depending on n and sd
+    mu_sd = sd/np.sqrt(n)
+    sample_mu = np.random.normal(loc=mu, scale=mu_sd)
+
+    # sample sd from distribution depending on n and sd
+    sd_sd = sd/np.sqrt(2*n)
+    sample_sd = np.random.normal(loc=sd, scale=sd_sd)
+    return dict(mu=sample_mu, sd=sample_sd, r=sample_r)
+
+
+def create_correlated_var(x, mu=0.0, sd=1.0, r=0.9, empirical=False):
+    """
+    Creates a random normally distributed array with the specified correlati`on
+    :param x: existing array to correlate to
+    :param mu: desired mean of the returned array
+    :param sd: desired stdev of the returned vector
+    :param r: desired correlation between existing and returned vectors
+    :param empirical: if true, mu, sd, and r specify the empirical not the population mean
+    """
+    n = len(x)
+    if not empirical:
+        sample_params=sample_from_population(n, mu, sd, r)
+        mu = sample_params['mu']
+        sd = sample_params['sd']
+        r = sample_params['r']
+
+    x = scale(x)
+    y = np.random.normal(size=n)
+    e = stats.OLS(y, x).fit().resid
+    z = r * scale(x) + np.sqrt(1-r**2) * scale(e)
+    return mu + sd * z
